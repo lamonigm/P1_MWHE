97e9cd42a7e9e955192c30d22acb4d0f3ec92d45
Eva Gallego <evagallegoortega@gmail.com>
2024-10-27 12:49:59 +0100

    Add base menu

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+$(document).ready(function() {
+      //OPACIDAD DEL MENU
+  
+      $(window).scroll (function(){
+        let scroll = $(this).scrollTop();
+        let menu = $("#menu").height();
+        let max_scroll = $(this).height() - menu;
+        
+        if (scroll <= max_scroll) {
+            let opacity = scroll / max_scroll;
+            $("#menu").css("background-color", "rgb(252, 182, 188, " + opacity + ")").css("box-shadow", "0 0 4px 2px rgba(0, 0, 0, 0.1)")
+        }
+      });
+  });
