9bac9b91d95262d9f6640b0290ff471361241286
Eva Gallego <evagallegoortega@gmail.com>
2024-11-06 12:27:23 +0100

    Menú en Proceso/ ajuste footer

--- js/proceso.js
+++ js/proceso.js
@@ -3,6 +3,29 @@ console.log("hello world");
 $(document).ready(function() {
       //OPACIDAD DEL MENU
   
+      // $(window).scroll (function(){
+      //   let scroll = $(this).scrollTop();
+      //   let menu = $("#menu").height();
+      //   let max_scroll = $(this).height() - menu;
+        
+      //   if (scroll <= max_scroll) {
+      //       let opacity = scroll / max_scroll;
+      //       $("#menu").css("background-color", "rgb(81, 158, 159, " + opacity + ")").css("box-shadow", "0 0 4px 2px rgba(0, 0, 0, 0.1)")
+      //   }
+      // });
+
+      // $(window).on("scroll", function() {
+      //   if ($(document).scrollTop() < 100) {
+      //     $(".nav-link").css({
+      //       textShadow: "0px 0px 5px var(--darkest__blue)" // Aplica el text-shadow
+      //     });
+      //   } else {
+      //     $(".nav-link").css({
+      //       textShadow: "none" // Elimina el text-shadow
+      //     });
+      //   }
+      // });
+
       $(window).scroll (function(){
         let scroll = $(this).scrollTop();
         let menu = $("#menu").height();
@@ -15,16 +38,18 @@ $(document).ready(function() {
       });
 
       $(window).on("scroll", function() {
-        if ($(document).scrollTop() < 100) {
-          $(".nav-link").css({
-            textShadow: "0px 0px 5px var(--darkest__blue)" // Aplica el text-shadow
-          });
-        } else {
-          $(".nav-link").css({
-            textShadow: "none" // Elimina el text-shadow
-          });
-        }
+        scrollFunction();
       });
+      
+      function scrollFunction() {
+          if ($(document).scrollTop() > 200) {
+              $("#menu").addClass("scrolled");         // Añade clase para cambiar colores en navbar
+              $(".blur").addClass("blur-hidden");     // Añade clase para ocultar el blur
+          } else {
+              $("#menu").removeClass("scrolled");     // Quita clase para restaurar colores originales
+              $(".blur").removeClass("blur-hidden");  // Quita clase para restaurar el blur
+          }
+      }
 });
 
 document.addEventListener("DOMContentLoaded", function() {
